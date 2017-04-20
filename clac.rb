7c1ed5ce38c120f3f25b26c55361a4e94bfbb800
Michel Martens <michel@soveran.com>
2017-04-20 17:30:29 +0200

    Update to clac 0.2.1

--- clac.rb
+++ clac.rb
@@ -1,8 +1,8 @@
 class Clac < Formula
   desc "A command line, stack-based calculator with postfix notation"
   homepage "https://github.com/soveran/clac"
-  url "https://github.com/soveran/clac/archive/0.2.0.tar.gz"
-  sha256 "371776f8a8ce02cfb9edb3ada2dcc637396323ca831a61ddad38dfa8fe4d8920"
+  url "https://github.com/soveran/clac/archive/0.2.1.tar.gz"
+  sha256 "8cd15e387441b372f68216888d00dfaaf2e486bf6622b2eb7f122487d2476087"
 
   def install
     system "make", "PREFIX=#{prefix}", "install"
