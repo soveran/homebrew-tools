a45e36e96922cebba75cf35312d33bab13a87d6c
Michel Martens <michel@soveran.com>
2017-09-19 23:24:08 +0200

    Fix clac formula according to Homebrew guidelines

--- clac.rb
+++ clac.rb
@@ -1,5 +1,5 @@
 class Clac < Formula
-  desc "A command line, stack-based calculator with postfix notation"
+  desc "Command-line, stack-based calculator with postfix notation"
   homepage "https://github.com/soveran/clac"
   url "https://github.com/soveran/clac/archive/0.3.0.tar.gz"
   sha256 "a7a2397fdbac93924ca83c72e2df2027029f54c8921b813230208dd167ed0c20"
@@ -9,6 +9,6 @@ class Clac < Formula
   end
 
   test do
-    assert_equal "7", shell_output('clac "3 4 +"').strip
+    assert_equal "7", shell_output("#{bin}/clac '3 4 +'").strip
   end
 end
